b5ca048904a50ad31f55353ef1968d33c4ecfef5
León Luna <leon.lunna@gmail.com>
2021-12-19 10:33:42 -0300

    ✨ add date to hello world

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -27,6 +27,7 @@ const count = ref(0);
   </p>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
+  <p>{{ Date() }}</p>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
